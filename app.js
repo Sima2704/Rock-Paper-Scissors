b9a3f48598b77e43b60f0718170bc392ba4991fa
Sima Vlad <128591054+Sima2704@users.noreply.github.com.>
2024-11-21 21:17:29 +0200

    Add event listeners to buttons and make the logic of the game

--- app.js
+++ app.js
@@ -10,6 +10,46 @@ const scissorsBtn = document.querySelector('#scissors');
 const options = ['rock', 'paper', 'scissors'];
 
 const getComputerChoise = () => {
-    const choise = Math.floor(Math.random() * options.length());
+    const choise = Math.floor(Math.random() * options.length);
     return options[choise];
 }
+
+const playGame = (humanChoise, computerChoise) => {
+
+    if(humanScore < 5 && computerScore < 5){
+        if(humanChoise === 'rock' && computerChoise === 'rock'){
+            console.log('It is a draw!');
+            // draw(); 
+        }else if(humanChoise === 'rock' && computerChoise === 'scissors'){
+            console.log('You win!');
+            // win();
+        }else if(humanChoise === 'rock' && computerChoise === 'paper'){
+            console.log('You lose!');
+            // lose();
+        }else if(humanChoise === 'paper' && computerChoise === 'rock'){
+            console.log('You win!');
+            // win();
+        }else if(humanChoise === 'paper' && computerChoise === 'scissors'){
+            console.log('You lose!');
+            // lose();
+        }else if(humanChoise === 'scissors' && computerChoise === 'paper'){
+            console.log('You win!');
+            // win();
+        }else if(humanChoise === 'scissors' && computerChoise === 'rock'){
+            console.log('You lose!');
+            // lose();
+        }
+    }
+}
+
+rockBtn.addEventListener('click', (e) => {
+    playGame(e.target.id, getComputerChoise())
+})
+
+papaerBtn.addEventListener('click', (e) => {
+    playGame(e.target.id, getComputerChoise())
+})
+
+scissorsBtn.addEventListener('click', (e) => {
+    playGame(e.target.id, getComputerChoise())
+})
